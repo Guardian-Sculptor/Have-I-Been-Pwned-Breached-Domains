00d9a2fba9af631e8b09ffc6e3efc2e1cf8e8573
cyberhub-programmer <sai2021v@gmail.com>
2024-08-02 13:55:51 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 HIBP_API_KEY= ""
-HIBP_LINK = "https://haveibeenpwned.com/api/v3/breacheddomain/"
+HIBP_Breached_Domains_Link = "https://haveibeenpwned.com/api/v3/breacheddomain/"
+HIBP_Subscribed_Domains_Link = "https://haveibeenpwned.com/api/v3/subscribeddomains"
 HIBP_headers = {'hibp-api-key': HIBP_API_KEY }
