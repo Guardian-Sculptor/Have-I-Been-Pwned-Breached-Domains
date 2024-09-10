9294f5f8484b33be2a3c0f13cbce2a784fba1b59
Guardian-Sculptor <sai2021v@gmail.com>
2024-09-10 17:57:10 -0400

    Update hibp.py

--- hibp.py
+++ hibp.py
@@ -3,7 +3,8 @@ import json
 import time
 import datetime
 import csv
-from config import HIBP_headers, HIBP_Breached_Domains_Link, HIBP_Subscribed_Domains_Link
+import os
+HIBP_HEADERS = {'hibp-api-key': os.environ['HIBP_API_KEY'] }
 
 def export_to_csv(data,domain,writer):
     for email_id, values in data.items():
@@ -11,16 +12,25 @@ def export_to_csv(data,domain,writer):
         writer.writerow([email_address, ', '.join(values)])
 
 def HIBP_call(domain,writer):
-    response = requests.get(f"{HIBP_Breached_Domains_Link}{domain}", headers=HIBP_headers)
-    if response.status_code == 200:
-        data = response.json()
-        export_to_csv(data,domain,writer)
-    else:
-        print(f"Error on {domain}: {response.status_code}")
+    response = requests.get(f"{os.environ['HIBP_BREACHED_DOMAINS_LINK']}{domain}", headers=HIBP_HEADERS)
+    while True :
+        if response.status_code == 200:
+            data = response.json()
+            export_to_csv(data,domain,writer)
+            break
+        elif response.status_code == 429:
+            retry_time = response.headers.get('retry-after', 5)
+            print("sleep requested for" + retry_time )
+            time.sleep(retry_time + 1)
+            
+        else:
+            print(f"Error on {domain}: {response.status_code}")
+            print(response.text)
+            break
 
 def get_domains():
     domains = []
-    response = requests.get(f"{HIBP_Subscribed_Domains_Link}", headers=HIBP_headers)
+    response = requests.get(os.environ['HIBP_SUBSCRIBED_DOMAINS_LINK'], headers=HIBP_headers)
     if response.status_code == 200:
         data = response.json()
         for line in data:
@@ -44,4 +54,4 @@ if __name__ == "__main__":
     for domain in domains:
         HIBP_call(domain,writer)
         print(domain + " completed")
-        time.sleep(5)
+        
