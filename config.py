0d26bc8381db83f456d8d65a6c2b6d64926404fd
Guardian-Sculptor <sai2021v@gmail.com>
2024-09-10 18:02:59 -0400

    Update and rename config.py to config.yml

--- config.py
+++
@@ -1,4 +0,0 @@
-HIBP_API_KEY= ""
-HIBP_Breached_Domains_Link = "https://haveibeenpwned.com/api/v3/breacheddomain/"
-HIBP_Subscribed_Domains_Link = "https://haveibeenpwned.com/api/v3/subscribeddomains"
-HIBP_headers = {'hibp-api-key': HIBP_API_KEY }
